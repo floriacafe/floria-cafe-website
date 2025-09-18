437e22e1ef22e26f7ab671a70db937a5e65b656e
moecode-c <mohammedessameldincs@gmail.com>
2025-09-18 07:48:55 +0300

    space

--- reactjs/src/pages/Contact.jsx
+++ reactjs/src/pages/Contact.jsx
@@ -132,6 +132,13 @@ const Contact = () => {
           </div>
         </div>
       </div>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
     
     </>
